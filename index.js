9a1b909f2bc4ff0294451f35daae46fe34ecf7bd
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-28 18:16:31 -0400

    put questions in async function, created switch case to handle shape choice

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-const {prompt}= require('inquirer');
-const fs = require('fs');
-const shapes = require('./lib/shapes');
-const questions = require( './lib/questions');
+const fs = require("fs");
+const shapes = require("./lib/shapes");
+const questions = require("./lib/questions");
+
+function createLogo({ text, textColor, shape, shapeColor }) {
+  switch (shape) {
+    case "Circle":
+      return new Circle(text, textColor, shape, shapeColor);
+    case "Triangle":
+        return new Circle(text, textColor, shape, shapeColor);
+    case "Square":
+        return new Circle(text, textColor, shape, shapeColor);
+  }
+}
