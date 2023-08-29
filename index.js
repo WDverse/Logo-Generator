344e5fb4783d894287ba5143958740554edc3965
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-29 03:54:54 -0400

    fixed switch case, fixed validation

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
+const inquirer = require("inquirer");
 const fs = require("fs");
-const shapes = require("./lib/shapes");
-const questions = require("./lib/questions");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+const questions = require('./lib/questions');
 
-function createLogo({ text, textColor, shape, shapeColor }) {
+async function createLogo() {
+  const answers = await inquirer.prompt(questions.userQuestions());
+
+  const { text, textColor, shape, shapeColor } = answers;
+
   switch (shape) {
     case "Circle":
       return new Circle(text, textColor, shape, shapeColor);
     case "Triangle":
-      return new Circle(text, textColor, shape, shapeColor);
+      return new Triangle(text, textColor, shape, shapeColor);
     case "Square":
-      return new Circle(text, textColor, shape, shapeColor);
+      return new Square(text, textColor, shape, shapeColor);
     default:
+      throw new Error("Invalid shape!");
   }
 }
