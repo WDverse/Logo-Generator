42db5c4717ba0c994a5f7c1c6b4e2f203964c388
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-29 04:36:45 -0400

    updated createdLogo function to use dotthen

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Triangle, Square } = require("./lib/shapes");
-const questions = require('./lib/questions');
+const questions = require("./lib/questions");
 
-async function createLogo() {
-  const answers = await inquirer.prompt(questions.userQuestions());
+function createLogo() {
+  inquirer.prompt(questions.userQuestions())
+  .then((answers) => {
+    const { text, textColor, shape, shapeColor } = answers;
 
-  const { text, textColor, shape, shapeColor } = answers;
-
-  switch (shape) {
-    case "Circle":
-      return new Circle(text, textColor, shape, shapeColor);
-    case "Triangle":
-      return new Triangle(text, textColor, shape, shapeColor);
-    case "Square":
-      return new Square(text, textColor, shape, shapeColor);
-    default:
-      throw new Error("Invalid shape!");
-  }
+    switch (shape) {
+      case "Circle":
+        return new Circle(text, textColor, shape, shapeColor);
+      case "Triangle":
+        return new Triangle(text, textColor, shape, shapeColor);
+      case "Square":
+        return new Square(text, textColor, shape, shapeColor);
+      default:
+        throw new Error("Invalid shape!");
+    }
+  });
 }
+
+createLogo();
+
+// Once you are calling the function then the issue you're going to encounter is the terminal ends immediately after displaying the questions. This is because the createLogo function is asynchronous, but you're not waiting for it to complete before the Node.js process exits. You need to wait for the createLogo function to finish before allowing the script to exit. You can achieve this by using await or .then().
