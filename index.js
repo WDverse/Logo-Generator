47832e62515efe4b3d958ef36d9a8d7322a36607
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-29 03:09:30 -0400

    applied formatting

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function createLogo({ text, textColor, shape, shapeColor }) {
     case "Circle":
       return new Circle(text, textColor, shape, shapeColor);
     case "Triangle":
-        return new Circle(text, textColor, shape, shapeColor);
+      return new Circle(text, textColor, shape, shapeColor);
     case "Square":
-        return new Circle(text, textColor, shape, shapeColor);
+      return new Circle(text, textColor, shape, shapeColor);
     default:
   }
 }
