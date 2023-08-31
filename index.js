5d5cd97510aa4995a16bdf068718a453d2166b65
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-08-31 18:04:42 -0400

    fixed switch case and imports to write logo, exported questions, refactored circle clclass, fixed tests to pass circle test

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Triangle, Square } = require("./lib/shapes");
-const { userQuestions } = require("./lib/questions");
-
-function createLogo() {
-  inquirer.prompt(userQuestions()).then((answers) => {
-    const { text, textColor, shape, shapeColor } = answers;
-
-    switch (shape) {
-      // assign each shape to the same variable name and return it outside of the switch statement.
-      case "Circle":
-        return new Circle(text, textColor, shape, shapeColor);
-      case "Triangle":
-        return new Triangle(text, textColor, shape, shapeColor);
-      case "Square":
-        return new Square(text, textColor, shape, shapeColor);
-      default:
-        throw new Error("Invalid shape!");
-    }
-  });
-  // writeToFile(filename, answers);
+const { questions } = require("./lib/questions");
+const { writeFile } = require("fs/promises");
+async function createLogo() {
+  const { text, textColor, shape, shapeColor } = await inquirer.prompt(
+    questions
+  );
+  let finalShape;
+  switch (shape) {
+    case "Circle":
+      finalShape = new Circle(text, textColor, shapeColor);
+      break;
+    case "Triangle":
+      finalShape = new Triangle(text, textColor, shapeColor);
+      break;
+    case "Square":
+      finalShape = new Square(text, textColor, shapeColor);
+      break;
+    default:
+      throw new Error("Invalid shape!");
+  }
+  writeFile("logo.svg", finalShape.render());
+  console.log(finalShape.render());
 }
-
-const filename = `logo.svg`;
-
-// function writeToFile(filename, answers) {
-//   fs.writeFile(filename, userQuestions(answers), (err) =>
-//   err ? console.log(err) : console.log(`${filename} created!`)
-//   );
-// }
 
 createLogo();
