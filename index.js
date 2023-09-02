3187a9cd0c834c52650d108e419572171f33845f
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-02 03:20:59 -0400

    added comments

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+// Import required modules and classes.
 const inquirer = require("inquirer");
 const fs = require("fs");
 const { Circle, Triangle, Square } = require("./lib/shapes");
 const { questions } = require("./lib/questions");
 const { writeFile } = require("fs/promises");
+
+// Asynchronous function to create a logo based on user input.
 async function createLogo() {
+  // Prompt the user with a series of questions to gather logo details.
   const { text, textColor, shape, shapeColor } = await inquirer.prompt(
     questions
   );
-  let finalShape;
+
+  let finalShape; // Variable to hold the final logo shape.
+
+  // Use a switch statement to determine which shape class to instantiate based on user's choice.
   switch (shape) {
     case "Circle":
       finalShape = new Circle(text, textColor, shapeColor);
@@ -21,9 +28,14 @@ async function createLogo() {
     default:
       throw new Error("Invalid shape!");
   }
-  writeFile("logo.svg", finalShape.render());
+
+  // Write the rendered logo as an SVG file named "logo.svg".
+  await writeFile("logo.svg", finalShape.render());
+
+  // Display a success message to the user.
   console.log("\n");
   console.log("SVG Logo Created!");
 }
 
+// Call the createLogo function to initiate logo creation based on user input.
 createLogo();
